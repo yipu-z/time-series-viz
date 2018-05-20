c0f50b3972c5cb63554f70a9c1f828c736b29d33
Sebastianopazo <so2453@tc.columbia.edu>
2018-05-20 16:35:54 -0400

    fixing UI thread problems

--- visualizer.js
+++ visualizer.js
@@ -57,15 +57,18 @@ function logOut() {
 // =============================================================================
 // Read from database
 // =============================================================================
-var firebaseSubjectRef = firebase.database().ref();
-firebaseSubjectRef.on('value', function(datasnapshot) {
-  console.log('here');
-  for (var i = 0; i < Object.keys(datasnapshot.val()).length; i++) {
-    //Add each entry to subject selection
-    var subjectId = Object.keys(datasnapshot.val())[i];
-    $('#subjectSelect').append('<option value="' + subjectId + '">' + subjectId + '</option>');
-  }
-});
+function initializeSelection(){
+  var firebaseSubjectRef = firebase.database().ref();
+  firebaseSubjectRef.on('value', function(datasnapshot) {
+    console.log('here');
+    for (var i = 0; i < Object.keys(datasnapshot.val()).length; i++) {
+      //Add each entry to subject selection
+      var subjectId = Object.keys(datasnapshot.val())[i];
+      $('#subjectSelect').append('<option value="' + subjectId + '">' + subjectId + '</option>');
+    }
+  });
+}
+
 
 //Initialize bootstrap modal
 $('#subjectModal').on('shown.bs.modal', function() {
